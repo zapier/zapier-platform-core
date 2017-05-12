11eace1c0f5fed9c466492b1834d6b0341e0b617
Bruno Bernardino <bruno@bernardino.io>
2017-05-12 15:59:36 +0100

    Move to promise-based approach

--- src/tools/create-locker.js
+++ src/tools/create-locker.js
@@ -2,15 +2,17 @@
 
 const _ = require('lodash');
 
+const ZapierPromise = require('./promise');
+
 const LOCK_MAX_TIMEOUT = 60 * 60 * 24; // 1 day, in seconds
 
 // Designed to be some user provided function/api.
 const createLocker = (input) => {
   const rpc = _.get(input, '_zapier.rpc');
 
-  return (callback, options) => {
+  return (options) => {
     if (!rpc) {
-      throw new Error('rpc is not available');
+      return ZapierPromise.reject(new Error('rpc is not available'));
     }
 
     if (!options) {
@@ -22,30 +24,31 @@ const createLocker = (input) => {
       name: 'single-lock'
     };
 
+    // We're nice, so we allow a lock without settings
     _.assign(options, defaultOptions);
 
     if (!options.name) {
-      throw new Error('Lock name needs to be defined.');
+      return ZapierPromise.reject(new Error('Lock name needs to be defined.'));
     }
 
     if (options.timeout > LOCK_MAX_TIMEOUT) {
-      throw new Error(`Timeout for lock is too big. Max is ${LOCK_MAX_TIMEOUT}.`);
+      return ZapierPromise.reject(new Error(`Timeout for lock is too big. Max is ${LOCK_MAX_TIMEOUT}.`));
     }
 
     const isRunningOnTrigger = _.get(input, '_zapier.event.method', '').indexOf('triggers.') === 0;
     const isRunningOnSearch = _.get(input, '_zapier.event.method', '').indexOf('searches.') === 0;
     const isRunningOnHydrator = _.get(input, '_zapier.event.method', '').indexOf('hydrators.') === 0;
     if (isRunningOnSearch || isRunningOnTrigger || isRunningOnHydrator) {
-      throw new Error('Cannot lock inside a trigger, search, or hydration function/method.');
+      return ZapierPromise.reject(new Error('Cannot lock inside a trigger, search, or hydration function/method.'));
     }
 
     return rpc('run_with_lock', options)
       .then((result) => {
         if (result.available) {
-          callback();
+          return true;
         } else {
           // TODO: Do we throw something else here for the runner to catch it?
-          throw new Error('NOT_A_REAL_ERROR_JUST_TRY_ME_AGAIN');
+          return ZapierPromise.reject(new Error('NOT_A_REAL_ERROR_JUST_TRY_ME_AGAIN'));
         }
       });
   };
