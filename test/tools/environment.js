f40e0312cb26c1ad4536590a5b3996144740da59
David Brownman <beamneocube@gmail.com>
2018-02-27 16:00:36 -0800

    read from .env by default in inject()

--- test/tools/environment.js
+++ test/tools/environment.js
@@ -1,30 +1,55 @@
-require('should');
+const should = require('should');
 const mock = require('mock-fs');
 
 const tools = require('../../src/tools/exported');
-const fake_file = 'CITY="boulder"\nNAME="david"\nPIZZA="Blackjack"\n';
+let env;
 
-describe('read env', () => {
+describe.only('read env', () => {
   beforeEach(() => {
-    mock({
-      '.environment': fake_file,
-      secrets: 'SECRET=very_secret_thing'
-    });
+    env = Object.assign({}, process.env);
   });
 
-  it('should parse a config', done => {
+  it('should should only read .env', done => {
+    mock({
+      '.environment': 'CITY="boulder"\nNAME="david"\n',
+      '.env': 'PIZZA="Blackjack"\n',
+      secrets: 'SECRET=very_secret_thing'
+    });
+
     tools.env.inject();
     process.env.PIZZA.should.equal('Blackjack');
+    should.not.exist(process.env.CITY);
     done();
   });
 
-  it('should parse a with filename', done => {
+  // this is temporary in v6, removed for v7
+  it('should read .environment if .env is missing', done => {
+    mock({
+      '.environment': 'CITY="boulder"\nNAME="david"\n'
+    });
+
+    tools.env.inject();
+    process.env.CITY.should.equal('boulder');
+    should.not.exist(process.env.PIZZA);
+    done();
+  });
+
+  it('should parse a with filename, ignore defaults', done => {
+    mock({
+      '.environment': 'CITY="boulder"\nNAME="david"\n',
+      '.env': 'PIZZA="Blackjack"\n',
+      secrets: 'SECRET=very_secret_thing'
+    });
+
     tools.env.inject('secrets');
     process.env.SECRET.should.equal('very_secret_thing');
+    should.not.exist(process.env.CITY);
+    should.not.exist(process.env.PIZZA);
     done();
   });
 
   afterEach(() => {
     mock.restore();
+    process.env = env;
   });
 });
