97a27e34b9534c8472ba3e34864c09b24a0f29a6
Chang-Hung Liang <chang-hung.liang@zapier.com>
2018-05-16 23:37:51 +0800

    Legacy Scripting: More sophisticated trigger tests

--- test/legacy-scripting/example-app.js
+++ test/legacy-scripting/example-app.js
@@ -6,9 +6,37 @@ const legacyScriptingSource = `
           params: { api_key: 'secret' }
         });
         var contacts = z.JSON.parse(response.content);
-        contacts[0].name = 'Patched by Legacy Scripting!';
+        contacts[0].name = 'Patched by KEY_poll!';
         return contacts;
-      }
+      },
+
+      contact_pre_pre_poll: function(bundle) {
+        bundle.request = {
+          url: 'https://auth-json-server.zapier.ninja/users',
+          params: { api_key: 'secret', id: 3 }
+        };
+        return bundle.request;
+      },
+
+      contact_post_post_poll: function(bundle) {
+        var contacts = z.JSON.parse(bundle.response.content);
+        contacts[0].name = 'Patched by KEY_post_poll!';
+        return contacts;
+      },
+
+      contact_pre_post_pre_poll: function(bundle) {
+        bundle.request = {
+          url: 'https://auth-json-server.zapier.ninja/users',
+          params: { api_key: 'secret', id: 4 }
+        };
+        return bundle.request;
+      },
+
+      contact_pre_post_post_poll: function(bundle) {
+        var contacts = z.JSON.parse(bundle.response.content);
+        contacts[0].name = 'Patched by KEY_pre_poll & KEY_post_poll!';
+        return contacts;
+      },
     };
 `;
 
@@ -25,10 +53,56 @@ const ContactTrigger_full = {
   }
 };
 
+const ContactTrigger_pre = {
+  key: 'contact_pre',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Pre Scripting'
+  },
+  operation: {
+    perform: {
+      source: "return z.legacyScripting.run(bundle, 'trigger', 'contact_pre');"
+    }
+  }
+};
+
+const ContactTrigger_post = {
+  key: 'contact_post',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Post Scripting'
+  },
+  operation: {
+    legacyProps: {
+      url: 'https://auth-json-server.zapier.ninja/users?api_key=secret'
+    },
+    perform: {
+      source: "return z.legacyScripting.run(bundle, 'trigger', 'contact_post');"
+    }
+  }
+};
+
+const ContactTrigger_pre_post = {
+  key: 'contact_pre_post',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Pre & Post Scripting'
+  },
+  operation: {
+    perform: {
+      source:
+        "return z.legacyScripting.run(bundle, 'trigger', 'contact_pre_post');"
+    }
+  }
+};
+
 const App = {
   title: 'Example App',
   triggers: {
-    [ContactTrigger_full.key]: ContactTrigger_full
+    [ContactTrigger_full.key]: ContactTrigger_full,
+    [ContactTrigger_pre.key]: ContactTrigger_pre,
+    [ContactTrigger_post.key]: ContactTrigger_post,
+    [ContactTrigger_pre_post.key]: ContactTrigger_pre_post
   },
   legacyScriptingSource
 };
--- test/legacy-scripting/legacy-scripting.js
+++ test/legacy-scripting/legacy-scripting.js
@@ -6,7 +6,7 @@ const appDefinition = require('./example-app');
 const createApp = require('../../src/create-app');
 const createInput = require('../../src/tools/create-input');
 
-describe('legacy-scripting', () => {
+describe('legacy scripting', () => {
   const testLogger = (/* message, data */) => {
     // console.log(message, data);
     return Promise.resolve({});
@@ -24,13 +24,46 @@ describe('legacy-scripting', () => {
   };
 
   describe('triggers', () => {
-    it.skip('should run KEY_poll', () => {
+    it('KEY_poll', () => {
       const input = createTestInput('triggers.contact_full.operation.perform');
       return app(input).then(output => {
         output.results.length.should.greaterThan(1);
 
         const firstContact = output.results[0];
-        should.equal(firstContact.name, 'Patched by Legacy Scripting!');
+        should.equal(firstContact.name, 'Patched by KEY_poll!');
+      });
+    });
+
+    it('KEY_pre_poll', () => {
+      const input = createTestInput('triggers.contact_pre.operation.perform');
+      return app(input).then(output => {
+        output.results.length.should.equal(1);
+
+        const contact = output.results[0];
+        should.equal(contact.id, 3);
+      });
+    });
+
+    it('KEY_post_poll', () => {
+      const input = createTestInput('triggers.contact_post.operation.perform');
+      return app(input).then(output => {
+        output.results.length.should.greaterThan(1);
+
+        const firstContact = output.results[0];
+        should.equal(firstContact.name, 'Patched by KEY_post_poll!');
+      });
+    });
+
+    it('KEY_pre_poll & KEY_post_poll', () => {
+      const input = createTestInput(
+        'triggers.contact_pre_post.operation.perform'
+      );
+      return app(input).then(output => {
+        output.results.length.should.equal(1);
+
+        const contact = output.results[0];
+        should.equal(contact.id, 4);
+        should.equal(contact.name, 'Patched by KEY_pre_poll & KEY_post_poll!');
       });
     });
   });
