7902727d4b2e1734b7d5a3aba4a1cde803b68f25
Chang-Hung Liang <chang-hung.liang@zapier.com>
2018-09-24 20:48:55 +0800

    Better naming

--- src/tools/default-file-hydrator.js
+++ src/tools/default-file-hydrator.js
@@ -4,9 +4,15 @@ const defaultFileHydrator = (z, bundle) => {
   const request = bundle.inputData.request || {};
   request.url = bundle.inputData.url || request.url;
   request.raw = true;
-  const stream = z.request(request);
+
+  const filePromise = z.request(request);
   const meta = bundle.inputData.meta || {};
-  return z.stashFile(stream, meta.knownLength, meta.filename, meta.contentType);
+  return z.stashFile(
+    filePromise,
+    meta.knownLength,
+    meta.filename,
+    meta.contentType
+  );
 };
 
 module.exports = defaultFileHydrator;
