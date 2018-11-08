3ae813912c1ac5094f07978b01a96a30819a492b
Owen Evans <owen@bgeek.net>
2018-11-09 09:01:10 +1300

    add comments

--- src/app-middlewares/after/callback-status-catcher.js
+++ src/app-middlewares/after/callback-status-catcher.js
@@ -3,7 +3,9 @@ let ensureOutputEnvelope = require('../../tools/envelope').ensureOutputEnvelope;
 let STATUSES = require('../../constants').STATUSES;
 const _ = require('lodash');
 /*
-
+this method creates the correct envelope responses if the app has used a callback url in their code
+by signalling to Zapier that this app/method is returning a callback status the task will be placed
+in a waiting state until the callback is called.
 */
 let callbackStatusCatcher = output => {
   const input = output.input || {};
